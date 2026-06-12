b690d0da8a1b2223c5827611a57b5b4df0e516a2
Yohannes Mamo <calebbenyofoni2@gmail.com>
2026-06-13 02:38:09 +0300

    Deploy frontend update from main

--- backend/models/SystemConfig.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, String
-from database import Base
-
-class SystemConfig(Base):
-    __tablename__ = 'SystemConfig'
-    
-    ConfigKey = Column(String(50), primary_key=True, index=True)
-    ConfigValue = Column(String)
-    Description = Column(String)
